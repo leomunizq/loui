929682b23d0c2e6129c1a9a8f91211f32e5cec27
Leonardo Muniz <leonardo.mdq@hotmail.com>
2024-04-16 22:23:46 +0200

    Refactor font loading and SplashScreen logic in _layout.jsx

---
+++ app/_layout.tsx
@@ -0,0 +1,59 @@
+import { SplashScreen, Stack } from 'expo-router'
+import {
+  useFonts,
+  Inter_100Thin,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
+import { useEffect } from 'react'
+
+SplashScreen.preventAutoHideAsync()
+
+const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    Inter_100Thin,
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Inter_900Black
+  })
+
+  useEffect(() => {
+    if (error) throw error
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded, error])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
+  if (!fontsLoaded && !error) {
+    return null
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false
+        }}
+      />
+    </Stack>
+  )
+}
+
+export default RootLayout
