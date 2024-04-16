9fef73c7e8b5a30e9a22c15696f6842790ae7812
Leonardo Muniz <leonardo.mdq@hotmail.com>
2024-04-16 23:12:14 +0200

    Update font color and link in index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 import { Link } from 'expo-router'
+
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl font-pblack">Lousssss!</Text>
+      <Text className="text-2xl font-iblack">Lousssss!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: 'blue' }}>
-        proifle
+      <Link href="/home" style={{ color: 'blue' }}>
+        home
       </Link>
     </View>
   )
